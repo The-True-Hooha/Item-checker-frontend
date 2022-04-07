b376f7b4ec469b127c95c78ddc5168ae57c266f5
David Ogar <owogogahhero@outlook.com>
2022-04-07 04:05:43 +0100

    defined routers for add-items list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListItemsComponent } from './components/list-items/list-items.component';
 
 import { HttpClientModule} from '@angular/common/http';
 import { AddItemsComponent } from './components/add-items/add-items.component';
+
+
+//defined variable for the router
+const routers: Routes = [
+  {path: 'items', component: ListItemsComponent},
+  {path: 'addItems', component: AddItemsComponent},
+  {path: '', redirectTo: '/items', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
